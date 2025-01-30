3ad448cfb9b295dfee5095bcf41308ba09ae921a
abl <yu950713@gmail.com>
2025-01-31 03:42:56 +0800

    feat: add an api to trigger historical data sync with startBlock and endBlock params

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ethers } from 'ethers';
+import { syncTransactions } from './services/sync';
 // import { getTransactions } from './services/transaction';
 
 const router = express.Router();
@@ -8,13 +10,12 @@ router.get('/health', (req, res) => {
   res.json({ message: 'Server is running!' });
 });
 
-
 // POST /transactions 
 router.post('/transactions', async (req, res) => {
   try {
     // Destructure query and pagination parameters from the request body
     const { startTimestamp, endTimestamp, txHash, page = 1, limit = 50 } = req.body;
-// console.log( startTimestamp, endTimestamp, txHash, page, limit)
+    // console.log( startTimestamp, endTimestamp, txHash, page, limit)
     // Ensure valid numbers for page and limit
     const pageNumber = Number.isNaN(parseInt(page)) ? 1 : Math.max(parseInt(page, 10), 1);
     const pageSize = Number.isNaN(parseInt(limit)) ? 50 : Math.max(Math.min(parseInt(limit, 10), 100), 1); // Max limit = 100
@@ -338,6 +339,30 @@ router.post('/transactions', async (req, res) => {
   } catch (error) {
     console.error('Error fetching transactions:', error);
     res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// POST /sync
+router.post('/sync', async (req, res) => {
+  try {
+    const { startBlock, endBlock } = req.body;
+
+    const infuraApiKey = process.env.INFURA_API_KEY;
+    if (!infuraApiKey) {
+      throw('INFURA_API_KEY is not defined.');
+    }
+    const provider = new ethers.JsonRpcProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`);
+
+    const startBlockParam = startBlock ? parseInt(startBlock, 10) : 0;
+    const endBlockParam = endBlock ? parseInt(endBlock, 10) : await provider.getBlockNumber();;
+
+    // Call the sync service
+    const totalTransactions = await syncTransactions(startBlockParam, endBlockParam);
+
+    res.json({ message: 'Transactions synced successfully', total: totalTransactions });
+  } catch (error) {
+    console.error('Error syncing transactions:', error);
+    res.status(500).json({ error });
   }
 });
 
---
+++ src/services/sync.ts
@@ -0,0 +1,86 @@
+import fetch from 'node-fetch';
+import { EtherscanResponse } from '../types/etherscan';
+// import { Queue } from 'bullmq';
+
+// const transactionQueue = new Queue('transaction-queue', {
+//   connection: {
+//     host: 'localhost',
+//     port: 6379
+//   }
+// });
+
+const numOfItemsPerBatch = 10000;
+
+// Fetch token transactions from Etherscan API
+async function fetchTokenTransactions(startBlock: number, endBlock: number) {
+  const poolAddress = process.env.UNISWAP_POOL_ADDRESS;
+  if (!poolAddress) {
+    throw ('UNISWAP_POOL_ADDRESS is not defined.');
+  }
+
+  const etherscanApi = process.env.ETHERSCAN_API_KEY;
+  if (!etherscanApi) {
+    throw ('ETHERSCAN_API_KEY is not defined.');
+  }
+
+  const url = `https://api.etherscan.io/api?module=account&action=tokentx&address=${poolAddress}&page=1&offset=${numOfItemsPerBatch}&startblock=${startBlock}&endblock=${endBlock}&sort=asc&apikey=${etherscanApi}`;
+  console.log(url)
+  const response = await fetch(url);
+  const data = await response.json() as EtherscanResponse;
+
+  if (data.status !== '1') {
+    throw new Error(`Etherscan API error: ${data.message}`);
+  }
+
+  return data.result;
+}
+
+// Sync transactions between block range
+export async function syncTransactions(startBlock: number, endBlock: number): Promise<number> {
+  let currentStartBlock = startBlock;
+  let totalSynced = 0;
+
+  try {
+    while (true) {
+      console.log(`Fetching transactions from block ${currentStartBlock} to ${endBlock}...`);
+
+      // Fetch transactions
+      const transactions = await fetchTokenTransactions(currentStartBlock, endBlock);
+
+      if (transactions.length === 0) {
+        console.log('No more transactions found.');
+        break;
+      }
+
+      // Process transactions
+      for (const tx of transactions) {
+        const transactionData = {
+          hash: tx.hash,
+          block_number: parseInt(tx.blockNumber, 10),
+          timestamp: parseInt(tx.timeStamp, 10),
+          gas_used: tx.gasUsed,
+          gas_price: tx.gasPrice
+        };
+
+        // console.log('Publishing transaction:', transactionData);
+
+        // Publish message to the queue
+        // await transactionQueue.add('record-transaction', transactionData);
+      }
+
+      totalSynced += transactions.length;
+
+      // Check if more data might exist
+      if (transactions.length < numOfItemsPerBatch) {
+        break;  // No more data to fetch
+      }
+
+      // Update currentStartBlock to the last transaction's block number
+      currentStartBlock = parseInt(transactions[transactions.length - 1].blockNumber, 10) + 1;
+    }
+  } catch (error) {
+    console.error('Error fetch historical transactions:', error);
+  }
+  console.log(`Sync complete. Total transactions synced: ${totalSynced} and ended at block: ${currentStartBlock}`);
+  return totalSynced;
+}
