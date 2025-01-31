848ef711838dbf55ea9b11dc8f1343454d6ad826
abl <yu950713@gmail.com>
2025-01-31 14:16:03 +0800

    feat: setup postgres connection

---
+++ src/db.ts
@@ -0,0 +1,11 @@
+// database connection
+
+import { Pool } from 'pg';
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+});
