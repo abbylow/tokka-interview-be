d2b1c926e876762394eb0787b22a06d5fc34f60d
abl <yu950713@gmail.com>
2025-01-31 18:20:23 +0800

    feat: change schema to store the calculated values

--- init.sql
+++ init.sql
@@ -6,6 +6,8 @@ CREATE TABLE IF NOT EXISTS transactions (
     gas_used NUMERIC(32, 0) NOT NULL,
     gas_price NUMERIC(32, 0) NOT NULL,
     eth_price_at_tx NUMERIC(32, 6) NOT NULL,
+    eth_fee NUMERIC(32, 18) GENERATED ALWAYS AS (gas_used * gas_price / 1e18) STORED,  -- ETH fee stored value
+    usdt_fee NUMERIC(32, 6) GENERATED ALWAYS AS ((gas_used * gas_price / 1e18) * eth_price_at_tx) STORED, -- USDT fee stored value
     processed_at BIGINT DEFAULT EXTRACT(EPOCH FROM NOW())::BIGINT
 );
 
